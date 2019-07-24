f23d110f49b0147ef02d3570c89f39a56f09d3c1
vzakusylo <ados@bk.ru>
2019-07-25 00:10:53 +0300

    133 check bracket using stack

---
+++ lafore_data_structures/133-brackets/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace _133_brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            while (true)
+            {
+                Console.WriteLine("Enter string containing delimiters:");
+                var input = Console.ReadLine();
+                BracketChecker theChecker = new BracketChecker(input);
+                theChecker.check();
+            }
+        }
+    }
+}
