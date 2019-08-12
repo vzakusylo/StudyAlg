eb2637593f0bf772a42e6c282aed8cb699374d68
vzakusylo <ados@bk.ru>
2019-08-12 21:34:59 +0300

    197 first last list

---
+++ lafore_data_structures/197-first-last-list/FirstLastList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _197_first_last_list
+{
+    public class FirstLastList
+    {
+        private Link first;
+        private Link last;
+
+        public FirstLastList()
+        {
+        }
+
+        public bool IsEmpty()
+        {
+            return first == null;
+        }
+
+        public void InsertFirst(long dd)
+        {
+            Link newLink = new Link(dd);
+            if (IsEmpty())
+            {
+                last = newLink;
+            }
+
+            newLink.next = first;
+            first = newLink;
+        }
+
+        public void InsertLast(long dd)
+        {
+            Link newLink = new Link(dd);
+            if (IsEmpty())
+            {
+                first = newLink;
+            }
+            else
+            {
+                last.next = newLink;
+            }
+
+            last = newLink;
+        }
+
+        public long DeleteFirst()
+        {
+            long temp = first.dData;
+            if (first.next == null)
+            {
+                last = null;
+            }
+
+            first = first.next;
+            return temp;
+        }
+
+        public void DisplayList()
+        {
+            Console.Write("List (first->last): ");
+            Link current = first;
+            while (current != null)
+            {
+                current.displayLink();
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ lafore_data_structures/197-first-last-list/Link.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _197_first_last_list
+{
+    public class Link
+    {
+        public long dData;
+        public Link next;
+
+        public Link(long d)
+        {
+            dData = d;
+        }
+
+        public void displayLink()
+        {
+            Console.Write($"{dData} ");
+        }
+    }
+}
