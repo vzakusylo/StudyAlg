421915c436f381b1683656bfb73556dd0006ab76
vzakusylo <ados@bk.ru>
2019-09-04 23:34:22 +0300

    moved project to test

--- lafore_data_structures/Book/Part05-linked-list/190-linked-list.cs
+++ lafore_data_structures/Book/Part05-linked-list/190-linked-list.cs
@@ -1,7 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace _190_linked_list
+namespace Page190
 {
+    [TestClass]
+    public class ListStack
+    {
+        [TestMethod]
+        public void Main()
+        {
+            LinkList list = new LinkList();
+            list.InsertFirst(22, 2.99);
+            list.InsertFirst(44, 44.99);
+            list.InsertFirst(66, 66.99);
+            list.InsertFirst(88, 88.99);
+
+            list.DisplayList();
+
+            Console.WriteLine();
+            Link f = list.find(44);
+            Console.WriteLine(f != null ? $"Found link with key {f.iData}" : "Can't find link");
+
+            Link d = list.delete(66);
+            Console.WriteLine(d != null ? $"Deleted link with key {d.iData}" : "Can't delete link");
+
+            while (!list.IsEmpty())
+            {
+                Link link = list.DeleteFirst();
+                Console.Write("Deleted: ");
+                link.DisplayLink();
+                Console.WriteLine();
+            }
+
+            list.DisplayList();
+        }
+    }
+
+    public class Link
+    {
+        public int iData;
+        public double dData;
+        public Link next;
+
+        public Link(int id, double dd)
+        {
+            iData = id;
+            dData = dd;
+        }
+
+        public void DisplayLink()
+        {
+            Console.Write(ToString());
+        }
+
+        public override string ToString()
+        {
+            return $"{'{'}{iData}, {dData}{'}'} ";
+        }
+    }
+
     public class LinkList
     {
         private Link _first;
