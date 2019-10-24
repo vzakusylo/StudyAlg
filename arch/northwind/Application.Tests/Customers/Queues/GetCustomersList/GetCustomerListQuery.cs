7907dd29069eccc457f31a9aaa782275ac9274f9
vadym zakusylo <v.zakusylo@intetics.com>
2019-10-24 19:30:42 +0300

    query command

---
+++ arch/northwind/Application.Tests/Customers/Queues/GetCustomersList/GetCustomerListQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Application.Tests.Customers.Queues.GetCustomersList
+{
+    public class GetCustomerListQuery : IRequest<CustomersListViewModel>
+    {
+    }
+}
---
+++ arch/northwind/Application.Tests/Customers/Queues/GetCustomersList/GetCustomersListQueueHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace Application.Tests.Customers.Queues.GetCustomersList
+{
+    public class GetCustomersListQueueHandler : IRequestHandler<GetCustomerListQuery, CustomersListViewModel>
+    {
+    }
+
+    public class CustomersListViewModel
+    {
+        public IList<>
+    }
+}
