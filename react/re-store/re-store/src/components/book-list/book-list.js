427a10f3c8d96fb6de13555349ab0e360d8bfa67
vadym zakusylo <v.zakusylo@intetics.com>
2019-10-28 15:06:35 +0200

    132 namin convention

--- react/re-store/re-store/src/components/book-list/book-list.js
+++ react/re-store/re-store/src/components/book-list/book-list.js
@@ -5,7 +5,7 @@ import ErrorIndicator from '../error-indicator';
 import './book-list.css';
 import {connect} from 'react-redux';
 import { withBookstoreService } from '../hoc';
-import {booksLoaded, booksRequested, booksError} from "../../actions";
+import {fetchBooks} from "../../actions";
 import {compose} from '../../utils';
 import './book-list.css';
 
@@ -43,16 +43,10 @@ const mapStateToProps = ({books, loading, error}) => {
     return { books, loading, error };
 };
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-    const { bookstoreService } = ownProps
+const mapDispatchToProps = (dispatch, { bookstoreService }) => {    
     return {             
-        fetchBooks : () => {
-            dispatch(booksRequested());
-            bookstoreService.getBooks()
-                .then((data) => dispatch(booksLoaded(data)))
-                .catch((err) => dispatch(booksError(err)));
-        } 
-    }
+        fetchBooks : fetchBooks(bookstoreService, dispatch)
+    };
 }
 
 export default compose(
