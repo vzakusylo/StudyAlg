23a4c4b621ff134bafb55b9c5854c18877f9ee00
vadosz <vadym.zakusylo@gmail.com>
2020-01-06 11:39:09 +0200

    design - starting adding optional object.

---
+++ design/oop-ood/making-code-more-object-oriented/08-optional-call-to-option-objects/01-making-optional-call.cs
@@ -0,0 +1,182 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Optional_Calls
+{
+    [TestClass]
+    public class Client
+    {
+        [TestMethod]
+        public void Main()
+        {
+            DateTime sellingDate = new DateTime(2019, 12, 9);
+            TimeSpan moneyBackSpan = TimeSpan.FromDays(30);
+            TimeSpan warrantySpan = TimeSpan.FromDays(365);
+
+            IWarranty moneyBack = new TimeLimitedWarranty(sellingDate, moneyBackSpan);
+            //IWarranty warranty = new TimeLimitedWarranty(sellingDate, warrantySpan);
+
+            //SoldArticle goods = new SoldArticle(moneyBack, warranty);
+            //  Offer money back
+            //  Offer repair
+
+            //SoldArticle goods = new SoldArticle(VoidWarranty.Instance, warranty);
+            // Offer repair
+            IWarranty warranty = new LifetimeWarranty(sellingDate);
+            SoldArticle goods = new SoldArticle(warranty, warranty);
+
+            ClaimWarrenty(goods);
+        }
+
+        // warning: beware of bool method arguments
+        // Their appearance is the indicator of leaving the OO design principles
+        private static void ClaimWarrenty(SoldArticle article)  // bool flags
+        {
+            DateTime now = DateTime.Now;
+            article.MoneyBackGuarantee.Claim(now, () => Console.WriteLine("Offer money back"));
+            article.ExpressWarranty.Claim(now, () => Console.WriteLine("Offer repair"));
+
+            //if (article.MoneyBackGuarantee.IsValidOn(now))
+            //{
+            //    Console.WriteLine("Offer money back");
+            //}
+            //if (article.ExpressWarranty.IsValidOn(now))
+            //{
+            //    Console.WriteLine("Offer repair");
+            //}
+        }
+    }
+
+    // New requirement: Some article have replaceable electrical parts. Circuitry is subject to separate warranty. 
+    public class SoldArticle
+    {
+        public IWarranty MoneyBackGuarantee { get; set; }
+        public IWarranty ExpressWarranty { get; set; }
+        public IWarranty NotOpperationalWarranty { get; set; }
+        private IWarranty CircuitryWarranty { get; set; }
+
+        public SoldArticle(IWarranty moneyBack, IWarranty express)
+        {
+            MoneyBackGuarantee = moneyBack;
+            ExpressWarranty = VoidWarranty.Instance;
+            NotOpperationalWarranty = express;
+            CircuitryWarranty = VoidWarranty.Instance;
+        }
+
+        public void VisibleDamage()
+        {
+            this.MoneyBackGuarantee = VoidWarranty.Instance;
+        }
+        public void NotOperational()
+        {
+            MoneyBackGuarantee = VoidWarranty.Instance;
+            ExpressWarranty = NotOpperationalWarranty;
+        }
+
+        private Part Circuitry { get; set; }
+        private IWarranty FailedCircuitryWarranty { get; set; }
+
+        public void CircuitryNotOpperational(DateTime detectedOn) 
+        {
+            Circuitry.MarkDefective(detectedOn);
+        }
+
+        public void InstallCircuitry(Part circuitry, IWarranty extendedWarranty)
+        {
+            Circuitry = circuitry;
+            FailedCircuitryWarranty = extendedWarranty;
+        }
+    }
+
+    public class Part
+    {
+        public DateTime InstallmentDate { get; }
+        public DateTime DefecDetectedOn { get; private set; }
+        public Part(DateTime instalmentDate)
+        {
+            InstallmentDate = instalmentDate;
+        }
+
+        public void MarkDefective(DateTime withDate)
+        {
+            DefecDetectedOn = withDate;
+        }
+    }
+
+    public interface IWarranty
+    {
+        // bool IsValidOn(DateTime date);
+
+        void Claim(DateTime onDate, Action onValidClaim);
+    }
+
+    public class TimeLimitedWarranty : IWarranty
+    {
+        private DateTime DateIssued { get; set; }
+        private TimeSpan Duration { get; set; }
+
+        public TimeLimitedWarranty(DateTime dateIssued, TimeSpan duration)
+        {
+            DateIssued = dateIssued.Date;
+            Duration = TimeSpan.FromDays(duration.Days);
+        }
+
+        public bool IsValidOn(DateTime date) =>
+            date.Date >= DateIssued &&
+            date.Date < DateIssued + Duration;
+
+        public void Claim(DateTime onDate, Action onValidClaim)
+        {
+            if (!IsValidOn(onDate))
+            {
+                return;
+            }
+            onValidClaim();
+        }
+    }
+
+
+    public class VoidWarranty : IWarranty // Null object class
+    {
+        [ThreadStatic] //Indicates that the value of a static field is unique for each thread.
+        private static VoidWarranty instance;
+        private VoidWarranty() { }
+        public static VoidWarranty Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new VoidWarranty();
+                }
+                return instance;
+            }
+        }
+
+        public void Claim(DateTime onDate, Action onValidClaim)
+        {
+
+        }
+
+        public bool IsValidOn(DateTime date) => false;
+    }
+
+    public class LifetimeWarranty : IWarranty // Special case class
+    {
+        private DateTime IssuingDate { get; }
+
+        public LifetimeWarranty(DateTime issuingDate) => IssuingDate = issuingDate;
+
+        public bool IsValidOn(DateTime date) => date >= IssuingDate;
+
+        public void Claim(DateTime onDate, Action onValidClaim)
+        {
+            if (!IsValidOn(onDate))
+            {
+                return;
+            }
+            onValidClaim();
+        }
+    }
+}
