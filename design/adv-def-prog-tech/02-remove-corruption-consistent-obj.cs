68f0b978b48b7f0a5bb6ecce45293790d7b394f4
Vadym Zakusylo <vadym.zakusylo@komplettbank.no>
2020-05-31 12:20:39 +0200

    design - defensive - avoid primitive type

--- design/adv-def-prog-tech/02-remove-corruption-consistent-obj.cs
+++ design/adv-def-prog-tech/02-remove-corruption-consistent-obj.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -41,9 +43,9 @@ namespace remove_corruption_consistent_obj
             Administrator != null &&
             Subject != null &&
             Candidate != null &&
-            Candidate.Enrolled != Subject.TaughDuring &&
+            Candidate.Enrolled != Subject.TaughtDuring &&
             !Candidate.PassedExam(Subject) &&
-            Subject.TaughBy == Administrator;
+            Subject.TaughtBy == Administrator;
 
         public IExamApplication Build()
         {
@@ -80,7 +82,7 @@ namespace remove_corruption_consistent_obj
 
     class RegularStudent : Student
     {
-        public RegularStudent(string name) : base(name) { }
+        public RegularStudent(PersonalName name) : base(name) { }
 
         public override bool CanEnroll(Semester semester)
         {
@@ -90,7 +92,7 @@ namespace remove_corruption_consistent_obj
 
     class ExchangeStudent : Student
     {
-        public ExchangeStudent(string name) : base(name) { }
+        public ExchangeStudent(PersonalName name) : base(name) { }
 
         public override bool CanEnroll(Semester semester)
         {
@@ -100,21 +102,35 @@ namespace remove_corruption_consistent_obj
 
     abstract class Student
     {
-        public string Name { get; }
+        public PersonalName Name { get; }
         public Semester Enrolled { get; private set; }
+        public Dictionary<Subject, Grade> Grades { get; }
 
-        public Student(string name)
-        {
-            if (string.IsNullOrEmpty(name))
-            {
-                throw new ArgumentException("Name can't be empty", nameof(name));
-            }
-
-            Name = name;
+        public Student(PersonalName name)
+        {           
+            Name = name ?? throw new ArgumentNullException(nameof(name));
         }
 
-        public abstract bool CanEnroll(Semester semester);
-       
+        public void AddGrade(Subject subject, Grade grade)
+        {
+            if (grade == null)            
+                throw new ArgumentException(nameof(grade));
+            if (subject == null || !IsEnlistedFor(subject))
+                throw new ArgumentException(nameof(grade));
+            if (Grades.ContainsKey(subject) && Grades[subject] != Grade.F)
+                throw new ArgumentException();
+
+            Grades[subject] = grade;
+        }
+
+        public double AverageGrade => Grades.Values
+            .Select(grade => grade.NumericEquivalent)
+            .Where(value => value > 0)
+            .Average();
+
+        private bool IsEnlistedFor(Subject subject) => true;
+
+        public abstract bool CanEnroll(Semester semester);       
 
         public void Enroll(Semester semester)
         {
@@ -158,6 +174,53 @@ namespace remove_corruption_consistent_obj
         }
     }
 
+    class PersonalName
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleNames { get; set; }
+
+        public PersonalName(string firstName, string middleNames, string lastName)
+        {
+            if (IsBadMandatoryPart(firstName) ||
+                IsBadOptionalPart(MiddleNames) ||
+                IsBadMandatoryPart(lastName))
+                throw new ArgumentException();
+
+            FirstName = firstName;
+            MiddleNames = middleNames;
+            LastName = lastName;
+        }
+
+        private bool IsBadOptionalPart(string part) => part == null ||
+            part.Length > 0 && char.IsHighSurrogate(part[part.Length - 1]);
+
+        private bool IsBadMandatoryPart(string part) =>
+            IsBadOptionalPart(part) || part == string.Empty;
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as PersonalName);
+        }
+
+        private bool Equals(PersonalName other) =>
+            other != null &&
+            ArePartsEqual(other.FirstName, FirstName) &&
+            ArePartsEqual(other.LastName, LastName) &&
+            ArePartsEqual(other.MiddleNames, MiddleNames);
+
+
+        public override int GetHashCode() =>
+            FirstName.GetHashCode() ^
+            MiddleNames.GetHashCode() ^
+            LastName.GetHashCode();
+
+        private bool ArePartsEqual(string part1, string part2) =>
+            string.Compare(part1, part2, StringComparison.OrdinalIgnoreCase) == 0;
+        // string.Equals(p.CountryCode, countryCode, StringComparison.OrdinalIgnoreCase)
+
+    }
+
     class Semester
     {
         public Semester Predecessor { get; internal set; }
@@ -167,8 +230,55 @@ namespace remove_corruption_consistent_obj
 
     class Subject
     {
-        public Semester TaughDuring { get; internal set; }
-        public Professor TaughBy { get; internal set; }
+        public Semester TaughtDuring { get; internal set; }
+        public Professor TaughtBy { get; internal set; }
+        public string Name { get; }
+        public List<Student> EnlistedStudents { get; private set; }
+
+        public Subject(string name, Semester taughtDuring, Professor taughtBy)
+        {
+            Name = name;
+            TaughtDuring = taughtDuring;
+            TaughtBy = taughtBy;
+        }
+
+        public void Enlist(Student student) {
+            if (student is null)
+            {
+                throw new ArgumentNullException(nameof(student));
+            }
+
+            EnlistedStudents.Add(student);
+        }
+
+        public void AssignGrades(IEnumerable<Tuple<PersonalName, Grade>> grades)
+        {
+            var listedGrades = grades
+                .Select(tuple => new
+                {
+                    Student = EnlistedStudents.First(student =>  // Subject's only responsibility is to assign grades, not to compare names.
+                        student.Name.Equals(tuple.Item1)),
+                    Grade = tuple.Item2
+                });
+
+            foreach (var studentGrade in listedGrades)
+            {
+                studentGrade.Student.AddGrade(this, studentGrade.Grade);
+            }
+        }
+    }
+
+    public class Grade
+    {
+        private Grade(double numericEquivalent) { NumericEquivalent = numericEquivalent; }
+
+        public double NumericEquivalent { get; }
+
+        public static Grade A { get; } = new Grade(4);
+        public static Grade B { get; } = new Grade(3);
+        public static Grade C { get; } = new Grade(2);
+        public static Grade D { get; } = new Grade(1);
+        public static Grade F { get; } = new Grade(0);
     }
 
 }
