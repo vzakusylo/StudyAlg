91be809fa3b6c7f354c5ea02b5729274b2aac31e
Vadym Zakusylo <vadym.zakusylo@komplettbank.no>
2020-05-31 16:11:12 +0200

    design - def prog - turning rule objects into active elements

--- design/adv-def-prog-tech/06-def-fun-domains-as-primary-line-of-defense/domain/IExamRules.cs
+++ design/adv-def-prog-tech/06-def-fun-domains-as-primary-line-of-defense/domain/IExamRules.cs
@@ -1,16 +1,40 @@
-﻿using System;
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace def_fun_domains_as_primary_line_of_defense
 {
+    public interface ICandidateSelector
+    {
+        IEnumerable<IExamApplication> Filter(IEnumerable<Student> candidates, IExam exam);
+    }
+
+    public class CandidateSelector : ICandidateSelector
+    {
+        public IEnumerable<IExamApplication> Filter([NotNull] IEnumerable<Student> candidates, [NotNull] IExam exam) =>
+            candidates
+                    .Where(candidate => CanApply(candidate, exam))
+                    .Select(candidate => 
+                        new Implementation.ExamApplication(
+                                exam.OnSubject, exam.AdministratedBy, candidate));
+
+        private bool CanApply(Student candidate, IExam exam)
+            => candidate.Enrolled == exam.OnSubject.TaughtDuring &&
+            !candidate.HasPassedExam(exam.OnSubject);
+    }
+
     public interface IExamRules
     {
-        bool CanApply(Student candidate, IExam exam);
-        IExamApplication Apply(Student student, IExam exam);
+       // bool CanApply(Student candidate, IExam exam);
+      //  IExamApplication Apply(Student candidate, IExam exam);
+
+        void WhenAplicable(Student candidate, IExam exam, Action<IExamApplication> action);
     }
 
     public class RegularExamRules : IExamRules
     {
-        public IExamApplication Apply(Student candidate, IExam exam)
+        private IExamApplication Apply(Student candidate, IExam exam)
         {
             if (!CanApply(candidate, exam))
                 throw new ArgumentException();
@@ -18,8 +42,20 @@ namespace def_fun_domains_as_primary_line_of_defense
             return new Implementation.ExamApplication(exam.OnSubject, exam.AdministratedBy, candidate);
         }
 
-        public bool CanApply(Student candidate, IExam exam)
+        private bool CanApply(Student candidate, IExam exam)
             => candidate.Enrolled == exam.OnSubject.TaughtDuring &&
             !candidate.HasPassedExam(exam.OnSubject);
+
+        public void WhenAplicable(
+            [NotNull] Student candidate, 
+            [NotNull] IExam exam, 
+            [NotNull] Action<IExamApplication> action)
+        {
+            if (CanApply(candidate, exam))
+            {
+                IExamApplication app = new Implementation.ExamApplication(exam.OnSubject, exam.AdministratedBy, candidate);
+                action(app);
+            }
+        }
     }
 }
