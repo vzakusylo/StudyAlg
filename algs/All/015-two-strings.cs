d408749623cc93e9e5e5cb922d3bdc550e84028b
Vadym Zakusylo <vadym.zakusylo@komplettbank.no>
2020-08-18 22:52:05 +0200

    alg hr

---
+++ algs/All/015-two-strings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace two_strings
+{
+    // https://www.hackerrank.com/challenges/two-strings/
+
+    [TestClass]
+    public class Program
+    {
+        [TestMethod]
+        public void Main()
+        {
+            var result = twoStrings("hello", "world");
+            Assert.AreEqual("YES", result);
+
+            result = twoStrings("hi", "world");
+            Assert.AreEqual("NO", result);
+        }
+
+        static string twoStrings(string s1, string s2)
+        {
+            return s1.ToArray().Distinct().Intersect(s2.ToArray().Distinct()).Any() ? "YES" : "NO";
+
+        }
+    }
+}
