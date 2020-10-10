cd7f7a64464081cc34c4ae0f23990d3fd01eed13
Vadym Zakusylo <vadym.zakusylo@komplettbank.no>
2020-10-11 00:28:05 +0200

    test deployment after change source code

--- arch/PayrollOnContainers/PayrollOnContainers/src/Services/Payroll/Payroll.API/Controllers/WeatherForecastController.cs
+++ arch/PayrollOnContainers/PayrollOnContainers/src/Services/Payroll/Payroll.API/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Payroll.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Vados", 
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
