b6fd86e60ecd2d3e271eebb019ec1e577e694577
Vadym Zakusylo <vadym.zakusylo@komplettbank.no>
2021-03-01 20:25:25 +0100

    design - kafka

---
+++ design/kafka/producer/Program.cs
@@ -0,0 +1,25 @@
+﻿using KafkaProducer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace kafka_prj
+{
+    //https://habr.com/ru/post/543732/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            CreateHostBuilder(args).Build().Run();
+            Console.ReadKey();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, collection) =>
+            collection.AddHostedService<KafkaProducerService>());
+    }
+}
