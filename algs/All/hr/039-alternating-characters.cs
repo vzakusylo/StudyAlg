c5d27f9dd93233ccb51f1865926771b9e0350f3d
Vadym Zakusylo <vadym.zakusylo@if.no>
2022-01-14 14:17:44 +0100

    alg hr str anagrams

---
+++ algs/All/hr/039-alternating-characters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace alternatingcharacters
+{
+    //https://www.hackerrank.com/challenges/alternating-characters
+
+    [TestClass]
+    public class Program
+    {
+        [TestMethod]
+        public void Main()
+        {
+            var s = "AAAA";
+            var res = alternatingCharacters(s);
+            Assert.AreEqual(3, res);
+        }
+
+        public static int alternatingCharacters(string s)
+        {
+            int counter = 0;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i] == s[i + 1])
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+    }
+}
---
+++ algs/All/hr/040-making-anagrams.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ctcimakinganagrams
+{
+    // https://www.hackerrank.com/challenges/ctci-making-anagrams
+
+    [TestClass]
+    public class Program
+    {
+        [TestMethod]
+        public void Main()
+        {
+           
+        }
+
+        public static int makeAnagram(string a, string b)
+        {
+            int min_deletions = 0;
+            int[] a_frequences = new int [26];
+            int[] b_frequences = new int [26];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                var curChar = a[i];
+                int charToInt = (int) curChar;
+                int position = charToInt - (int) 'a';
+                a_frequences[position]++;
+            }
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                var curChar = b[i];
+                int charToInt = (int)curChar;
+                int position = charToInt - (int)'a';
+                b_frequences[position]++;
+            }
+
+            for (int i = 0; i < 26; i++)
+            {
+                int diff = Math.Abs(a_frequences[i] - b_frequences[i]);
+                min_deletions += diff;
+            }
+
+            return min_deletions;
+        }
+
+    }
+}
