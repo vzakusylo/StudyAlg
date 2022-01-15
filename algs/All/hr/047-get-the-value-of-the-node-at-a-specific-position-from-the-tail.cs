3d4ad44a73ce91e0899db7693db337d7e8630be2
Vadym Zakusylo <vadym.zakusylo@if.no>
2022-01-15 15:31:09 +0100

    alg hr compare two linked list, get the value at a specific position from the tail.

---
+++ algs/All/hr/047-get-the-value-of-the-node-at-a-specific-position-from-the-tail.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using insertanodeataspecificposition;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace getthevalueofthenodeataspecificpositionfromthetail
+{
+    // https://www.hackerrank.com/challenges/get-the-value-of-the-node-at-a-specific-position-from-the-tail
+
+    [TestClass]
+    public class Program
+    {
+        [TestMethod]
+        public void Main()
+        {
+            var head = new SinglyLinkedListNode(1);
+            head.next = new SinglyLinkedListNode(2);
+            head.next.next = new SinglyLinkedListNode(3);
+            head.next.next.next = new SinglyLinkedListNode(4);
+            head.next.next.next.next = new SinglyLinkedListNode(5);
+            head.next.next.next.next.next = new SinglyLinkedListNode(6);
+            head.next.next.next.next.next.next = new SinglyLinkedListNode(7);
+            head.next.next.next.next.next.next.next = new SinglyLinkedListNode(8);
+            head.next.next.next.next.next.next.next.next = new SinglyLinkedListNode(9);
+            head.next.next.next.next.next.next.next.next.next = new SinglyLinkedListNode(0);
+
+            var res = getNode(head, 3);
+
+            Assert.AreEqual(7, res);
+        }
+
+        public static int getNode(SinglyLinkedListNode head, int positionFromTail)
+        {
+            SinglyLinkedListNode pointerNode = head;
+            int counter = 0;
+
+            while (head != null)
+            {
+                head = head.next;
+                if (counter < positionFromTail)
+                {
+                    counter++;
+                }
+                else
+                {
+                    pointerNode = pointerNode.next;
+                }
+            }
+
+            return pointerNode.data;
+
+        }
+
+    }
+}
