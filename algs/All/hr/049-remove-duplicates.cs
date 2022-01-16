f4e2e4ac490dfd07600f43e409c32827424c510d
Vadym Zakusylo <vadym.zakusylo@if.no>
2022-01-16 23:38:11 +0100

    alg remove duplicates

---
+++ algs/All/hr/049-remove-duplicates.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using insertanodeataspecificposition;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace deleteduplicatevaluenodesfromasortedlinkedlist
+{
+    // https://www.hackerrank.com/challenges/delete-duplicate-value-nodes-from-a-sorted-linked-list
+
+    [TestClass]
+    public class Program
+    {
+        [TestMethod]
+        public void Main()
+        {
+           
+        }
+
+        public static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode head)
+        {
+            // Write your code here
+            if (head == null) return head;
+
+            SinglyLinkedListNode newHead = head;
+
+            while (head.next != null)
+            {
+                if (head.data == head.next.data)
+                {
+                    head.next = head.next.next;
+                }
+                else
+                {
+                    head = head.next;
+                }
+            }
+
+            return newHead;
+        }
+
+
+    }
+}
