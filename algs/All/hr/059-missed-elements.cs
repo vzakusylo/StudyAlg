95fd11ab50a626fc5e43e92eb12258f077d4217f
Vadym Zakusylo <vadym.zakusylo@if.no>
2022-01-19 20:41:54 +0100

    alg hr, missed numbers, sherlock and array

---
+++ algs/All/hr/059-missed-elements.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace missingnumbers
+{
+    // https://www.hackerrank.com/challenges/missing-numbers
+
+    [TestClass]
+    public class Program
+    {
+        [TestMethod]
+        public void Main()
+        {
+            var arr = new List<int>() {203, 204, 205, 206, 207, 208, 203, 204, 205, 206};
+            var brr = new List<int>() {203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204};
+
+            var res = missingNumbers(arr, brr);
+            Assert.AreEqual(new List<int>{ 204, 205, 206 }, res);
+        }
+
+        public static List<int> missingNumbers(List<int> arr, List<int> brr)
+        {
+            Dictionary<int, int> aMap = new Dictionary<int, int>();
+            Dictionary<int, int> bMap = new Dictionary<int, int>();
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (aMap.ContainsKey(arr[i]))
+                {
+                    aMap[arr[i]] += 1;
+                }
+                else
+                {
+                    aMap[arr[i]] = 1;
+                }
+            }
+
+            for (int i = 0; i < brr.Count; i++)
+            {
+                if (bMap.ContainsKey(brr[i]))
+                {
+                    bMap[brr[i]] = bMap[brr[i]] + 1;
+                }
+                else
+                {
+                    bMap[brr[i]] = 1;
+                }
+            }
+
+            List<int> list = new List<int>();
+            foreach (var element in bMap.Keys)
+            {
+                if (aMap.ContainsKey(element))
+                {
+                    if (bMap[element] > aMap[element])
+                    {
+                        list.Add(element);
+                    }
+                }
+                else
+                {
+                    list.Add(element);
+                }
+            }
+
+            list.Sort();
+            return list;
+
+        }
+
+    }
+}
---
+++ algs/All/hr/060-sherlok-and-array.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace sherlockandarray
+{
+    // https://www.hackerrank.com/challenges/sherlock-and-array
+
+    [TestClass]
+    public class Program
+    {
+        [TestMethod]
+        public void Main()
+        {
+            // 1, 2, 3, 3
+            // totalSum: 1 + 2 + 3 + 3 = 9
+            // i = 0, a[i] = 1, 1 == 9 - 0 , 1 != 9 , leftSum += 1 (1), totalSum(9) -= 1 = 8
+            // i = 1, a[i] = 2, 2 == 8 - 1 , 2 != 8 , leftSum += 2 (3), totalSum(8) -= 2 = 6
+            // i = 2, a[i] = 3, 3 == 6 - 3 , 3 == 3 -------found
+            var arr = new List<int> {1, 2, 3, 3};
+            var res = balancedSums(arr);
+            Assert.AreEqual("YES", res);
+        }
+
+        public static string balancedSums(List<int> arr)
+        {
+            if (arr.Count == 1) return "YES";
+
+            int totalSum = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                totalSum += arr[i];
+            }
+
+            int leftSum = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (leftSum == (totalSum - arr[i]))
+                {
+                    return "YES";
+                }
+                leftSum += arr[i];
+                totalSum -= arr[i];
+            }
+
+            return "NO";
+
+        }
+
+
+    }
+}
